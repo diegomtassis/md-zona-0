3eeaefdded0941d0cd313ac26d3cd77acc68996f
diegomtassis <diego.mt.dev@gmail.com>
2023-08-01 21:18:13 +0200

    * showing a grid tilemap

--- src/main.c
+++ src/main.c
@@ -10,10 +10,13 @@
 #include "../inc/fwk/printer.h"
 #include "../inc/fwk/vdp_utils.h"
 #include "../inc/zona0_file.h"
+#include "../res/grid.h"
 
-//#include "../inc/fwk/logger.h"
+ //#include "../inc/fwk/logger.h"
 
 #define LOADING_TIME	3000
+
+static void initGrid();
 
 int main(bool hard) {
 
@@ -37,6 +40,8 @@ int main(bool hard) {
 
 	resetTileMemory();
 
+	initGrid();
+
 	while (1) {
 
 		// log_memory();
@@ -45,3 +50,22 @@ int main(bool hard) {
 
 	return 0;
 }
+
+
+
+static void initGrid() {
+
+	PAL_setPalette(PAL2, palette_grid.data, DMA);
+
+	u16 idx_tile_bg_grid = idx_tile_malloc;
+	u16 vram_idx = idx_tile_bg_grid;
+
+	VDP_loadTileSet(&tileset_grid, idx_tile_bg_grid, DMA);
+	vram_idx += tileset_grid.numTile;
+
+	Map *bg;
+	bg = MAP_create(&map_zona0_zona14, BG_B, TILE_ATTR_FULL(PAL2, FALSE, FALSE, FALSE, idx_tile_bg_grid));
+
+	MAP_scrollTo(bg, 0, 0);
+}
+
